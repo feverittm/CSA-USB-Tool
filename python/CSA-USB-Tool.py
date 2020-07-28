f5256c7f76fbf347e111ffa52374ee84b3bc01d6
Floyd Moore <floyd.moore@gmail.com>
2020-07-27 23:08:03 -0700

    wip2

--- python/CSA-USB-Tool.py
+++ python/CSA-USB-Tool.py
@@ -1,47 +1,71 @@
+import numpy as np
 import csv
 from pprint import pprint
 import requests
+import os
+from os import path
+import sys
+import hashlib
 
 
 d = {}
-def load_csv():
-	with open('FRCSoftware2020.csv') as f:
-		headers = [header.strip() for header in next(f).split(",")]
 
+
+def headers(f):
+	m_headers = [header.strip() for header in next(f).split(",")]
+	m_headers[0] = m_headers[0][1:]
+	return(m_headers)
+
+def bighash(filename):
+	with open(filename, "rb") as f:
+		file_hash = hashlib.md5()
+		while chunk := f.read(8192):
+			file_hash.update(chunk)
+	#print(file_hash.hexdigest())
+	return(file_hash.hexdigest())
+
+
+def load_csv(file):
+	with open(file) as f:
+		n_headers = headers(f)
 		for line in f:
 			values = [value.strip() for value in line.split(",")]
-			d[values[0]] = dict(zip(headers, values[0:]))
+			d[values[0]] = dict(zip(n_headers, values[0:]))
+	pprint(d)
 
-def fetch_file():
-	
-load_csv()
-for l in d:
-	z = d[l]
-	print(f'File: {z["FriendlyName"]}')
-	print(f'\tDownload url: {z["URL"]}')
-	print(f'\tto local Filename: {z["FileName"]}')
+def main():
+	load_csv('../FRCSoftware2020.csv')
+	for l in d:
+		z = d[l]
+		print(f'File: {z["FriendlyName"]}')
+		print(f'\tDownload url: {z["URL"]}')
+		print(f'\tto local Filename: {z["FileName"]}')
+		file_name = z["FileName"]
 
-	myfile = requests.get(z["URL"], stream = True)
-	
-	with open(z["FileName"], "wb") as Pypdf:
-		total_length = int(myfile.headers.get('content-length'))/1024
-		print(f'\tFile size {total_length}')
-		
-		break
-	# next make save-file a routine
+		if (path.exists(file_name)):
+			print(f'\tFile Exists')
+			digest = bighash(file_name)
+			if digest != z["MD5"]:
+				print(f'Hex MD5 mismatch!')
+				exit()
+		else:
+			with open(file_name, "wb") as f:
+				myfile = requests.get(z["URL"], stream=True)
+				total_length = int(myfile.headers.get('content-length'))
+				print(f'\tFile size {total_length}')
 
-		for chunk in myfile.iter_content(chunk_size = 1024*1024):
+				if total_length is None:  # no content length header
+					f.write(myfile.content)
+				else:
+					dl = 0
+					total_length = int(total_length)
+					for data in myfile.iter_content(chunk_size=4096):
+						dl += len(data)
+						f.write(data)
+						done = int(50 * dl / total_length)
+						sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
+						sys.stdout.flush()
+			break
 
-			if chunk:
-
-				Pypdf.write(chunk)
-
-	#open(z["FileName"], 'wb').write(myfile.content)
-
-	break
-
-
-#pprint(d)
-
-exit()
-
+if __name__== "__main__":
+   main()
